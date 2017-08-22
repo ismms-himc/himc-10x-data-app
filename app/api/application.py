0569d039697b0eeca9bda8368efd8ae5820b2f22
Melanie Davila <davilam91@gmail.com>
2017-08-22 17:06:21 -0400

    Fill out config.py

--- app/api/application.py
+++ app/api/application.py
@@ -1,1 +1,45 @@
 # main entry point of application
+
+import os
+import sys
+import json
+
+import flask
+from flask import request, Response
+
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+# TODO: below needed??
+# from flask.ext.cors import cross_origin
+
+# TODO: needed?
+# Default config vals 
+# THEME = 'default' if os.environ.get('THEME') is None else os.environ.get('THEME')
+# FLASK_DEBUG = 'false' if os.environ.get('FLASK_DEBUG') is None else os.environ.get('FLASK_DEBUG')
+
+# Create the Flask app
+app = Flask(__name__)
+
+app_settings = os.getenv(
+    'APP_SETTINGS',
+    'project.server.config.DevelopmentConfig'
+)
+
+app.config.from_object(app_settings)
+
+app_settings = os.getenv(
+    'APP_SETTINGS',
+    'project.server.config.DevelopmentConfig'
+)
+
+# TODO: this is different than himc db. ok?
+app.config.from_object(app_settings)
+
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+# TODO: implement this
+# from project.server.auth.views import auth_blueprint
+# app.register_blueprint(auth_blueprint)
--- app/config.py
+++ app/config.py
@@ -1,3 +1,42 @@
 # storing all the module configuration for different environments (local, staging 
 # and prod). You don't want to be messing with production data while developing 
 # locally!
+
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+postgres_local_base = 'postgresql://postgres:@localhost/'
+database_name = 'himc_10x_data_app'
+
+
+class BaseConfig:
+    """Base configuration."""
+    # TODO: change key below??
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious')
+    DEBUG = False
+    BCRYPT_LOG_ROUNDS = 13
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration."""
+    DEBUG = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration."""
+    DEBUG = True
+    TESTING = True
+    BCRYPT_LOG_ROUNDS = 4
+    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name + '_test'
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration."""
+    # TODO: change key below??
+    SECRET_KEY = 'my_precious'
+    DEBUG = False
+SQLALCHEMY_DATABASE_URI = 'postgresql:///example'
