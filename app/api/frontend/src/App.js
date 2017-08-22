8674e42eb538042960a963e9ae05c91c2894bb5b
Melanie Davila <davilam91@gmail.com>
2017-08-22 15:54:29 -0400

    Re-integrate jsonServerRestClient

--- app/api/frontend/src/App.js
+++ app/api/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Admin, Resource } from 'admin-on-rest';
+import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 
 import { SampleList } from './samples';
 
@@ -11,8 +11,19 @@ import { SampleList } from './samples';
 // TODO: consider adding "sample icon"
 // closest material-ui icon option can be found here: https://www.materialui.co/icon/opacity
 
+
+// URL for jsonServerRestClient
+////////////////////////////////
+// TODO: determine if better alternative to this hack
+var url = window.location.origin;
+
+if (url.indexOf('3000') > -1){
+  // when developing (on port 3000) use local flask (on port 5000)
+  url = 'http://localhost:5000';
+}
+
 const App = () => (
-    <Admin restClient={jsonServerRestClient('http://jsonplaceholder.typicode.com')}>
+    <Admin restClient={jsonServerRestClient(url)}>
         <Resource name="samples" list={SampleList} />
     </Admin>
 );
