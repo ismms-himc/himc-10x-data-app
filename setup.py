c35ee5e0b1dfff31e01cbddff1b74ec6b0977952
Melanie Davila <davilam91@gmail.com>
2017-08-28 15:24:34 -0400

    Create setup.py file as per http://flask.pocoo.org/docs/0.12/patterns/packages/. Note: flask run results in error

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='himc-10x-data-app',
+    packages=['app'], # TODO: or himc-10x-data-app?
+    include_package_data=True,
+    install_requires=[
+        'flask', # TODO: add more as needed
+    ],
+)
