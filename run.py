19d6ffe6e5b302e457d79e0c0f0147bc88ffcdea
Melanie Davila <davilam91@gmail.com>
2017-08-30 17:08:50 -0400

    Move app running logic to run.py

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 # launch web server locally
+from app.api import app
+
+if __name__ == '__main__':
+    # # no debug
+    # application.run(host='0.0.0.0')
+
+    # running in debug mode
+    app.run(host='0.0.0.0', debug=True)
