1c5c2a95aa795aa72f1a067754e0d2044d2a9d33
Melanie Davila <davilam91@gmail.com>
2017-08-31 16:54:29 -0400

    Debugging of "My Samples" duplicate IDs in progress

--- app/api/frontend/src/samples.js
+++ app/api/frontend/src/samples.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'admin-on-rest';
+import { List, Datagrid, TextField, Filter, TextInput } from 'admin-on-rest';
+// import { List, TextField, Filter, TextInput } from 'admin-on-rest';
+// import Datagrid from "/admin-on-rest-custom/Datagrid";
 
-// import { List, Datagrid, TextField, Filter, TextInput } from 'admin-on-rest';
 
 // TODO: check if unneeded "Add Filter" button appears on top
-
 
 // TODO: will this be an issue?
 // The first filter, 'q', takes advantage of a full-text functionality offered by 
 // JSONPlaceholder. It is alwaysOn, so it always appears on the screen. 
 
-{/* const SampleFilter = (props) => (
+const SampleFilter = (props) => (
     <Filter {...props}>
         <TextInput label="Search" source="q" alwaysOn />
     </Filter>
-); */}
+);
+
+
+// TODO: Reimplement the fat arrow syntax when debugging complete
 
 {/*export const SampleList = (props) => (
-    <List {...props} filters={<SampleFilter />}>
+    <List {...props} filters={<SampleFilter />} title="Samples">
         <Datagrid>
             <TextField source="sample_id" />
         </Datagrid>
@@ -28,7 +31,7 @@ export const SampleList = function(props) {
 	console.log("PROPS:");
 	console.log(props);
 	return (
-		<List {...props} >
+		<List {...props} filters={<SampleFilter />} title="My Samples">
 			<Datagrid>
 				<TextField source="sample_id" />
 			</Datagrid>
--- app/api/routes/sample/views.py
+++ app/api/routes/sample/views.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Blueprint
+from flask import request, Blueprint
 from app.api.models.sample import Sample
 import json
 
@@ -20,12 +20,21 @@ def get_samples():
     	# TODO: eventually modify this to send S3 info needed
     	sample_data.append({ "sample_id": sample.sample_id })
 
+    if 'q' in request.args:
+    	# perform search using list comprehensions
+    	query_string = request.args['q']
+    	# we make the search case-insensitive
+    	data_for_response = [x for x in sample_data if query_string.lower() in x['sample_id'].lower()]
+    else:
+    	# TODO: Return only the requested # of items
+    	data_for_response = sample_data[:10]
+
     print("Sample Data:")
-    print(sample_data)
+    print(data_for_response)
     print("\n")
     print("<h1> Samples Index </h1>")
-    response = flask.Response(json.dumps(sample_data))
-    response.headers.add('X-Total-Count', len(sample_data))
+    response = flask.Response(json.dumps(data_for_response))
+    response.headers.add('X-Total-Count', len(data_for_response))
     response.headers.add('Access-Control-Expose-Headers', 'X-Total-Count')
     print("RESPONSE:")
     print(response)
