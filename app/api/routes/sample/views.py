4fa0245ca8b3e5f0a9a65ec269916461faa5ac52
Melanie Davila <davilam91@gmail.com>
2017-08-31 12:58:16 -0400

    Add response with appropriate headers to /samples API endpoint

--- app/api/routes/sample/views.py
+++ app/api/routes/sample/views.py
@@ -1,12 +1,29 @@
 import flask
 from flask import Blueprint
+from app.api.models.sample import Sample
+import json
 
 sample_blueprint = Blueprint('sample_pages', __name__)
 
 @sample_blueprint.route('/samples', methods=['GET'])
 # @cross_origin() # in Nick's hack; TODO: confirm CSRF protection works
-def sample_index():
-    # TODO: render a login/signup page if user not logged in; otherwise, render
-    # the Samples Dashboard
-    print("IN sample_index FUNCTION")
-    return "<h1>These are the samples :) <h1>"
+# TODO: only show samples that belong to logged in user
+# Returns all samples belonging to logged in user. HIMC staff will have access
+# to all samples.
+def get_samples():
+    samples = Sample.query.all()
+    # TODO: better way to do this than sending array of json objects to frontend?
+    # maybe by changing the restClient in App.js?
+    # TODO: deal with when user filters search or only want to return requested # of items
+    sample_data = []
+    for sample in samples:
+    	# TODO: eventually modify this to send S3 info needed
+    	sample_data.append({ "sample_id": sample.sample_id })
+
+    print(sample_data)
+    print("<h1> Samples Index </h1>")
+    response = flask.Response(json.dumps(sample_data))
+    response.headers.add('X-Total-Count', len(sample_data))
+    response.headers.add('Access-Control-Expose-Headers', 'X-Total-Count')
+
+    return(response)
