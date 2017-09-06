e0d5d2c7191ed6c8442d4adac62db03b46621342
Melanie Davila <davilam91@gmail.com>
2017-09-06 17:08:26 -0400

    Add more advanced event listener in entry file for future integration

--- frontend/himc_10x_data_app.jsx
+++ frontend/himc_10x_data_app.jsx
@@ -1,8 +1,23 @@
 // Entry file
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome!</h1>, root);
 });
+
+// TODO: Once the above works, implement the below:
+
+{/* document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    let store;
+    if (window.currentUser) {
+      const preloadedState = { session: { currentUser: window.currentUser } };
+      store = configureStore(preloadedState);
+    } else {
+      store = configureStore();
+    }
+    ReactDOM.render(<Root store={store}/>, root);
+}); */}
