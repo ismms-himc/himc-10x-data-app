6de1be6b063a1efa5194e35d10b7e8add46ae13c
Melanie Davila <davilam91@gmail.com>
2017-09-06 13:13:56 -0400

    Add event listener to entry file

--- frontend/himc_10x_data_app.jsx
+++ frontend/himc_10x_data_app.jsx
@@ -1,1 +1,8 @@
 // Entry file
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome!</h1>, root);
+});
