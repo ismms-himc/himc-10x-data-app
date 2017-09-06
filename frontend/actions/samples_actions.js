bc9f0d50cfc897e9ce49c70f754c1303e714276f
Melanie Davila <davilam91@gmail.com>
2017-09-06 16:09:36 -0400

    Add samples actions

---
+++ frontend/actions/samples_actions.js
@@ -0,0 +1,27 @@
+export const RECEIVE_SAMPLES = 'RECEIVE_SAMPLES';
+export const LOAD_SAMPLES = 'LOAD_SAMPLES ';
+// export const RECEIVE_SAMPLE_ERRORS = 'RECEIVE_SAMPLE_ERRORS';
+
+import * as APIUtil from '../util/samples_api_util';
+
+export const loadSamples = () => ({
+  type: LOAD_SAMPLES
+});
+
+export const receiveSamples = function (samples) {
+  return({
+    type: RECEIVE_SAMPLES,
+    samples
+  });
+};
+
+// export const receiveSampleErrors = (errors) => ({
+//   type: RECEIVE_SAMPLE_ERRORS,
+//   errors
+// });
+
+export const requestSamples = () => (dispatch) => {
+	dispatch(loadSamples());
+	return APIUtil.fetchAllSamples()
+		.then(feedItems => dispatch(receiveSamples(samples)));
+};
