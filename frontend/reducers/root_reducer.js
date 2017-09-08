2d871ffa40ae2c4d0661f76e352225f16844ee8b
Melanie Davila <davilam91@gmail.com>
2017-09-08 13:19:43 -0400

    Add loading reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import samplesReducer from './samples_reducer';
+import loadingReducer from './loading_reducer';
 
 const rootReducer = combineReducers({
+  loading: loadingReducer,
   samples: samplesReducer
 });
 
