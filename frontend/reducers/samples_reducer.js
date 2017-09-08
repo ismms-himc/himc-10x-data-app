e53384653dc23f658c68beeb5ffa06e1eda1a46d
Melanie Davila <davilam91@gmail.com>
2017-09-08 13:19:54 -0400

    Parse JSON response from API in samples reducer

--- frontend/reducers/samples_reducer.js
+++ frontend/reducers/samples_reducer.js
@@ -1,12 +1,11 @@
-// import { merge, assign, values } from 'lodash';
-import {  RECEIVE_SAMPLES } from '../actions/samples_actions';
+import { merge } from 'lodash';
+import { RECEIVE_SAMPLES } from '../actions/samples_actions';
 
 export default function samplesReducer(state = {}, action) {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_SAMPLES:
-      return merge({}, action.samples);
-      
+      return merge({}, JSON.parse(action.samples));
   default:
     return state;
   }
