8fd06b154ee4a4b59f825d18d55e5ca72b4fd0c4
Melanie Davila <davilam91@gmail.com>
2017-09-08 13:19:00 -0400

    Add future implementation of configureStore

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,8 +5,13 @@ import rootReducer from '../reducers/root_reducer';
 
 // TODO: remove logger in production
 
+const configureStore = function () {
+  return createStore(rootReducer, applyMiddleware(logger));
+};
+
+{/*
 const configureStore = function (preloadedState = {}) {
   return createStore(rootReducer, preloadedState, applyMiddleware(logger, thunk));
-};
+};*/}
 
 export default configureStore;
