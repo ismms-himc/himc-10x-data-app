cc3c3b7f042c06bfd8491c8d40b44f323bf84599
Melanie Davila <davilam91@gmail.com>
2017-09-11 12:25:22 -0400

    Add route logic to app.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import NavigationContainer from './navigation/navigation_container';
-import Footer from './footer/footer';
+import SampleIndexContainer from './samples/index/sample_index_container';
+import { Route, Switch } from 'react-router-dom';
+import { Redirect } from 'react-router';
+import HomeContainer from './home/home_container';
+import { ProtectedRoute, RedirectLoggedInUserRoute } from '../util/route_util';
+// import Footer from './footer/footer';
+
+{/*TODO: replace the functions below. For testing purposes only: */}
+function redirectIfLoggedIn() {
+  return <Redirect to="/samples"/>
+}
+
+function requireLogIn() {
+  console.log('requiring login');
+}
 
 const App = function (props) {
   return(
@@ -10,10 +24,21 @@ const App = function (props) {
           { props.children }
       </div>
       <div className='app-footer'>
-        <Footer />
+      {/*  <Footer /> */}
       </div>
+      <Switch>
+        <RedirectLoggedInUserRoute path="/" component={HomeContainer}/>
+        <ProtectedRoute path="/samples" component={SampleIndexContainer}/>
+      </Switch>
     </div>
   );
 };
 
 export default App;
+
+{/*<Route path="/" component={ App }/>
+  <IndexRoute component={ HomeContainer } onEnter={redirectIfLoggedIn}/>
+<Route
+  path='/samples'
+  component={ SampleIndexContainer }
+  onEnter={ requireLogIn }/> */}
